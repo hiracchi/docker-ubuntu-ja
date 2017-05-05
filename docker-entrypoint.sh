a58813510be885ebc3afc3b6907b0ff9c5346d54
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2017-05-05 23:58:55 +0900

    fix: update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 if [ -z "$*" ]; then
     tail -f /dev/null
 else
-    echo "$@"
-    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
+    export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+    if [ x${DOCKER_WORKDIR} != x ]; then
+        if [ x${DOCKER_USER} != x ]; then
+            if [ x${DOCKER_GROUP} != x ]; then
+                chown -R "${DOCKER_USER}:${DOCKER_GROUP}" "${DOCKER_WORKDIR}"
+            else
+                chown -R "${DOCKER_USER}" "${DOCKER_WORKDIR}"
+            fi
+        fi
+        cd ${DOCKER_WORKDIR}
+    fi
+    
+    if [ x${DOCKER_USER} != x ]; then
+        sudo -u ${DOCKER_USER} "$*"
+    else
+        eval "$*"
+    fi
 fi
 
